3b45167543846c8c3bf0b9d27c3ca6d91a8f41f3
DreamWalkerXZ <dreamwalkerxz@outlook.com>
2020-03-16 11:28:00 +0800

    :bug: Made Wi-Fi configuration persistent
    Made Wi-Fi configuration persistent by changing the sysupgrade.conf

--- diy.sh
+++ diy.sh
@@ -4,3 +4,7 @@
 # Add Compile-ID(cid)
 cid=$(date "+%Y-%m-%d")
 sed -i "s/19.07-SNAPSHOT/19.07-SNAPSHOT-[${cid}]/g" package/default-settings/files/zzz-default-settings
+
+# Make Wi-Fi configuration persistent
+echo "\n# Make Wi-Fi configuration persistent" >> package/base-files/files/etc/sysupgrade.conf
+echo "/etc/wireless/" >> package/base-files/files/etc/sysupgrade.conf
