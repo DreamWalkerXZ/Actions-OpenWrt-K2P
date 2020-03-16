005c000bf7cf351c39bc98fcda4472a58588578c
DreamWalkerXZ <dreamwalkerxz@outlook.com>
2020-03-16 11:32:08 +0800

    :fire: Removed the example code in diy.sh
    Removed the example code in diy.sh

--- diy.sh
+++ diy.sh
@@ -1,6 +1,3 @@
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
 # Add Compile-ID(cid)
 cid=$(date "+%Y-%m-%d")
 sed -i "s/19.07-SNAPSHOT/19.07-SNAPSHOT-[${cid}]/g" package/default-settings/files/zzz-default-settings
