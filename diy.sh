51bcf98050e6bf37ef55549c099a1bf7377de3a5
DreamWalkerXZ <dreamwalkerxz@outlook.com>
2020-05-09 12:08:53 +0000

    :sparkles: Update version

--- diy.sh
+++ diy.sh
@@ -1,3 +1,3 @@
 # Add Compile-ID(cid)
 cid=$(date "+%Y-%m-%d")
-sed -i "s/R20.4.8/R20.4.8[${cid}]/g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/R20.5.9/R20.5.9[${cid}]/g" package/lean/default-settings/files/zzz-default-settings
