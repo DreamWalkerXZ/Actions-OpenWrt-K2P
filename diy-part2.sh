a397ddc3116f769022e020ac01f5305685c65aa9
DreamWalkerXZ <dreamwalkerxz@outlook.com>
2021-03-02 21:58:26 +0800

    Reborn

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,11 +15,4 @@ rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
 
 # Change opkg source
-echo "echo 'src/gz openwrt_core https://k2p-repo.dreamwalkerxz.workers.dev/k2p-repo/targets/ramips/mt7621/packages' > /etc/opkg/distfeeds.conf" >> package/lean/default-settings/files/zzz-default-settings
-echo "echo 'src/gz openwrt_base https://k2p-repo.dreamwalkerxz.workers.dev/k2p-repo/packages/mipsel_24kc/base' >> /etc/opkg/distfeeds.conf" >> package/lean/default-settings/files/zzz-default-settings
-echo "echo 'src/gz openwrt_freifunk https://k2p-repo.dreamwalkerxz.workers.dev/k2p-repo/packages/mipsel_24kc/freifunk' >> /etc/opkg/distfeeds.conf" >> package/lean/default-settings/files/zzz-default-settings
-echo "echo 'src/gz openwrt_helloworld https://k2p-repo.dreamwalkerxz.workers.dev/k2p-repo/packages/mipsel_24kc/helloworld' >> /etc/opkg/distfeeds.conf" >> package/lean/default-settings/files/zzz-default-settings
-echo "echo 'src/gz openwrt_luci https://k2p-repo.dreamwalkerxz.workers.dev/k2p-repo/packages/mipsel_24kc/luci' >> /etc/opkg/distfeeds.conf" >> package/lean/default-settings/files/zzz-default-settings
-echo "echo 'src/gz openwrt_packages https://k2p-repo.dreamwalkerxz.workers.dev/k2p-repo/packages/mipsel_24kc/packages' >> /etc/opkg/distfeeds.conf" >> package/lean/default-settings/files/zzz-default-settings
-echo "echo 'src/gz openwrt_routing https://k2p-repo.dreamwalkerxz.workers.dev/k2p-repo/packages/mipsel_24kc/routing' >> /etc/opkg/distfeeds.conf" >> package/lean/default-settings/files/zzz-default-settings
-echo "echo 'src/gz openwrt_telephony https://k2p-repo.dreamwalkerxz.workers.dev/k2p-repo/packages/mipsel_24kc/telephony' >> /etc/opkg/distfeeds.conf" >> package/lean/default-settings/files/zzz-default-settings
+echo "sed -i 's#https://mirrors.cloud.tencent.com/lede/snapshots#https://k2p-repo.dreamwalkerxz.workers.dev/k2p-repo#g' /etc/opkg/distfeeds.conf" >> package/lean/default-settings/files/zzz-default-settings
